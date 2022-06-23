e319f27367ae47512fa0cb423e2475b72f0b63f3
simonmortensen23 <simon020393@gmail.com>
2022-06-23 15:21:50 +0000

    Adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+  });
